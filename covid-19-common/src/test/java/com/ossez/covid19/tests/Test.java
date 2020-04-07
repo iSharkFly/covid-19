6f3a50f91578d64f9b2e0f59704179baf644b8b8
YuCheng Hu <yhu@ossez.com>
2020-04-07 16:59:00 -0400

    OSSEZ-45 add all test file to the PRJ

---
+++ covid-19-common/src/test/java/com/ossez/covid19/tests/Test.java
@@ -0,0 +1,12 @@
+package com.ossez.covid19.tests;
+
+import java.math.BigDecimal;
+
+public class Test {
+	@org.junit.Test
+	public void testNumber() {
+		BigDecimal bd = new BigDecimal(999);
+		
+		
+	}
+}
