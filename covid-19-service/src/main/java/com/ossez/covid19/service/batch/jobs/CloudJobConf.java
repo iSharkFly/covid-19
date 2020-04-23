ebc0a9bead093058aabedee3bb13c840aaa0ac83
YuCheng Hu <yhu@ossez.com>
2020-04-23 09:30:31 -0400

    New Job task

--- covid-19-service/src/main/java/com/ossez/covid19/service/batch/jobs/CloudJobConf.java
+++ covid-19-service/src/main/java/com/ossez/covid19/service/batch/jobs/CloudJobConf.java
@@ -3,6 +3,7 @@ package com.ossez.covid19.service.batch.jobs;
 import com.ossez.covid19.common.models.Listing;
 import com.ossez.covid19.service.batch.jobs.listener.JobCompletionNotificationListener;
 import com.ossez.covid19.service.batch.tasklet.AwsTasklet;
+import com.ossez.covid19.service.batch.tasklet.Covid19Tasklet;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 
@@ -34,36 +35,48 @@ public class CloudJobConf {
     }
 
     @Bean
-    public Job cloudClean(JobCompletionNotificationListener listener, Step deleteFilesStep) {
+    public Job cloudClean(JobCompletionNotificationListener listener, Step importDataStep) {
         return jobBuilderFactory.get("cloudClean")
                 .incrementer(new RunIdIncrementer())
                 .listener(listener)
-                .flow(deleteFilesStep)
+                .flow(importDataStep)
                 .end()
                 .build();
     }
 
-    @Bean
-    public Step stepAws(ItemReader<Listing> itemReader, ItemProcessor<Listing, Listing> itemProcessor, ItemWriter<Listing> itemWriter) {
-        return stepBuilderFactory.get("stepAws")
-                .<Listing, Listing>chunk(100)
-                .reader(itemReader)
-                .processor(itemProcessor)
-                .writer(itemWriter)
-                .build();
-    }
+//    @Bean
+//    public Step stepAws(ItemReader<Listing> itemReader, ItemProcessor<Listing, Listing> itemProcessor, ItemWriter<Listing> itemWriter) {
+//        return stepBuilderFactory.get("stepAws")
+//                .<Listing, Listing>chunk(100)
+//                .reader(itemReader)
+//                .processor(itemProcessor)
+//                .writer(itemWriter)
+//                .build();
+//    }
 
+    //
     @Bean
     public Step deleteFilesStep(StepBuilderFactory stepBuilders) {
         return stepBuilders.get("deleteFilesStep")
                 .tasklet(fileDeletingTasklet()).build();
     }
 
+    @Bean
+    public Step importDataStep(StepBuilderFactory stepBuilders) {
+        return stepBuilders.get("importDataStep").tasklet(covid19Tasklet()).build();
+    }
 
+
+    // TASKLET
     @Bean
     public AwsTasklet fileDeletingTasklet() {
         return new AwsTasklet(
                 new FileSystemResource("target/test-inputs"));
+    }
+
+    @Bean
+    public Covid19Tasklet covid19Tasklet() {
+        return new Covid19Tasklet(new FileSystemResource("target/test-inputs"));
     }
 
 }
